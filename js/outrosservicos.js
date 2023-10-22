44424dc74b95ae9efd5057116c158773a10b117d
Rodney Assis <rodney.assis@hotmail.com>
2023-10-21 23:11:32 -0300

    att 21/10/2023
    Tela portal
    responsividade da tela do portal
    iniciando a tela da versão mobile
    corrigindo erros da versão desktop
    
    tela Outros serviços e login
    criação dos efeitos utilizando js
    responsividade da tela de login
    adição dos botões de retroceder e login
    alteração de estilização dos botões da tela de outros serviços

---
+++ js/outrosservicos.js
@@ -0,0 +1,22 @@
+function extencao() {
+    let ext = window.document.getElementById("extencao");
+    extencao.style.transform = 'translateY(5px)';
+    
+}
+
+
+function boletos() {
+    let bol = window.document.getElementById("boletos");
+    boletos.style.transform = 'translateY(5px)';
+}
+
+
+function protocolo() {
+    let pro = window.document.getElementById("protocolo");
+    protocolo.style.transform = 'translateY(5px)';
+}
+
+function loguin() {
+    let entrar = window.document.getElementById("login");
+    login.style.transform = 'translateY(5px)';
+}
