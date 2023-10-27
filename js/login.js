442a24cc3268c8d6dfc6bab2248af14f6464bfea
flavia-aluna <flavia.mvieira@souunit.com.br>
2023-10-27 00:56:46 -0300

    Adição do JS na página de Login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function outrosservicos() {
+    let out = window.document.getElementById("outrosservicos")
+    outrosservicos.style.transform = 'translateY(5px)';
+}
+
+function login() {
+    let log = window.document.getElementById("button")
+    button.style.transform = 'translateY(5px)';
+}
