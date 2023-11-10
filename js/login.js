e656a45daefa63232f1c4362fa5b8389e55c0f04
flavia-aluna <flavia.mvieira@souunit.com.br>
2023-11-09 23:59:40 -0300

    adição de validação de login

--- js/login.js
+++ js/login.js
@@ -3,7 +3,22 @@ function outrosservicos() {
     outrosservicos.style.transform = 'translateY(5px)';
 }
 
-function login() {
+function move() {
     let log = window.document.getElementById("button")
-    button.style.transform = 'translateY(5px)';
+    move.style.transform = 'translateY(5px)';
+}
+
+function verificar() {
+    const usuarioInput = document.getElementById("txtn1").value;
+    const senhaInput = document.getElementById("txtn2").value;
+
+    const usuario = "usuario"; 
+    const senha = "senha123"; 
+
+    if (usuarioInput === usuario && senhaInput === senha) {
+       window.location.href = "../html/portal.html";
+       
+    } else {
+        window.alert("Usuário ou senha incorretos. Tente novamente!");
+    }
 }
