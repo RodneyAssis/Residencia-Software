49574230e5aa0d3acacdecc6c9b0e4a36fdf9ca9
Rodney Assis <rodney.assis@hotmail.com>
2023-11-29 17:17:58 -0300

    Att Geral
    Adição do tutorial nas demais paginas,
    Adição dos títulos nas paginas,
    Adição favicon para as demais paginas,
    Excluindo redundâncias.

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,10 +1,28 @@
-function abrirModal() {
-    const modal = document.getElementById("janela-modal")
-    modal.classList.add("abrir")
 
-    if (modal.style.display === "none") {
-        modal.style.display = "flex";
-    } else {
-        modal.style.display = "none";
+function abrialerta() {
+    Swal.fire({
+      title: "Tutorial",
+      html: `
+      <div class="video">
+        <video controls width="300" poster="../img/img_vídeos/vdimg 1.webp"> 
+          <source style="width: 500px;" src="../img/links_vídeos/video1.mp4">
+        </video>
+        <h4>Tutorial 1</h4>
+      </div>
+      <div class="video">
+        <video controls width="300" poster="../img/img_vídeos/vdimg 2.webp"> 
+          <source src="../img/links_vídeos/video1.mp4">
+        </video>
+        <h4>Tutorial 2</h4>
+      </div>
+      <div class="video">
+        <video controls width="300" poster="../img/img_vídeos/vdimg 3.webp"> 
+          <source src="../img/links_vídeos/video1.mp4">
+        </video>
+        <h4>Tutorial 3</h4>
+      </div>
+      `,
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Fechar"
+    })
     }
-}
